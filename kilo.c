7fa35a67f729586168b458f291974a6d77cf77fe
xyz5020 <xyzmail520@gmail.com>
2021-12-01 18:00:46 +0800

    update update/save function

--- kilo.c
+++ kilo.c
@@ -1,23 +1,690 @@
 //
 // Created by xuying on 2021/11/26.
 //
+
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
+/*** includes ***/
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <errno.h>
 #include <termios.h> //termios is unix api for terminal i/o
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include <unistd.h> // 提供unix环境下大量的system call wrapper functions
 
-void enableRowMode() {
-    struct termios raw; // 将变量raw设置为termios结构类型
-    tcgetattr(STDIN_FILENO, &raw); // 获取标准输入的属性，将其写入变量raw
-    raw.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+/*** define ***/
+#define CTRL_KEY(k) ((k) & 0x1f) // define: replacing text macro, 可以接受参数
+#define KILO_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
+
+/*** declaration prototypes ***/
+void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
+
+/*** data ***/
+struct termios orig_termios;
+
+typedef struct {
+  int size;
+  char *chars;
+  int rsize;
+  char *render;
+} erow;
+
+typedef struct {
+  int cx;
+  int cy;
+  int rx;
+  int rowoff; // row offset
+  int coloff; // 列偏移
+  int screenrows; // 屏幕行数
+  int screencols; // 屏幕宽度
+  int numrows; // 行数(文本行数)
+  int dirty; // 是否有未保存的更改
+  char *filename;
+  char statusMsg[80];
+  time_t statusMsgTime;
+  struct termios orig_termios;
+  erow *row;
+} editorConfig;
+
+editorConfig E;
+
+enum editorKey {
+  BACKSPACE = 127,
+  ARROW_UP = 1000,
+  ARROW_DOWN,
+  ARROW_LEFT,
+  ARROW_RIGHT,
+  DEL_KEY,
+  PAGE_UP,
+  PAGE_DOWN
+};
+
+/*** terminal ***/
+void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  perror(s);
+  exit(1);
 }
 
-int main() {
-    printf("program beginning");
-    enableRowMode();
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1) {
+    die("tcsetattr");
+  }
+}
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-    printf("hello,world\n");
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) {
+    die("tcgetattr");
+  } // 获取标准输入的属性，将其写入变量
+  atexit(&disableRawMode);
+
+  // 这里没有使用指针，那么值就是copy到raw变量上了，不会改变原变量的值
+  struct termios raw = E.orig_termios; // 将变量raw设置为termios结构类型, 并写入获取的属性
+  raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+  raw.c_oflag &= ~OPOST;
+  raw.c_cflag |= CS8;
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)) {
+    die("tcsetattr");
+  } // 设置stdin的属性
+}
+
+int editorReadKey() {
+  long nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+
+  if (c == '\x1b') {
+    char seq[3];
+    // read 有0.1s的间隔，只要在这个间隔内没有读到数据，则下面的条件就会触发
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+    if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '3':
+              return DEL_KEY;
+            case '5':
+              return PAGE_UP;
+            case '6':
+              return PAGE_DOWN;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A':
+            return ARROW_UP;
+          case 'B':
+            return ARROW_DOWN;
+          case 'C':
+            return ARROW_RIGHT;
+          case 'D':
+            return ARROW_LEFT;
+          default:
+            break;
+        }
+      }
+    }
+    return '\x1b';
+  } else {
+    return c;
+  }
+}
+
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
     return 0;
+  }
+}
+
+/*** row operations ***/
+
+int editorRowCxToRx(erow *row, int cx) {
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++) {
+    if (row->chars[j] == '\t') {
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+    }
+    rx++;
+  }
+  return rx;
+}
+
+void editorUpdateRow(erow *row) {
+  int tabs = 0;
+  for (int j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') tabs++;
+  }
+  free(row->render);
+  row->render = malloc(row->size + tabs * (KILO_TAB_STOP - 1) + 1);
+  int idx = 0;
+  for (int j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % KILO_TAB_STOP != 0) row->render[idx++] = ' ';
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) return;
+
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editorUpdateRow(&E.row[at]);
+  E.numrows++;
+  E.dirty++;
+}
+
+void editorAppendRow(char *s, size_t len) {
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  if (E.row == NULL) die("realloc");
+
+  // 初始化新的行
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  if (E.row[at].chars == NULL) die("malloc");
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editorUpdateRow(&E.row[at]);
+
+  E.numrows++;
+  E.dirty++;
+}
+
+void editorFreeRow(erow *row) {
+  free(row->render);
+  free(row->chars);
+}
+
+void editorDelRow(int at) {
+  if (at < 0 || at >= E.numrows) return;
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+  E.numrows--;
+  E.dirty++;
+}
+
+void editorRowInsertChar(erow *row, int at, int c) {
+  if (at < 0 || at > row->size) at = row->size;
+  row->chars = realloc(row->chars, row->size + 2);
+  // 光标所在当前位置的字符及其后面的所有字符向后移一位，在当前位置插入输入的新字符
+  memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+  row->size++;
+  row->chars[at] = c;
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
+void editorRowAppendString(erow *row, char *s, size_t len) {
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
+void editorRowDelChar(erow *row, int at) {
+  if (at < 0 || at >= row->size) return;
+  memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+  row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
+/*** editor operation ***/
+
+void editorInsertChar(int c) {
+  if (E.cy == E.numrows) {
+    editorInsertRow(E.numrows, "", 0);
+  }
+  // 编辑当前行
+  editorRowInsertChar(&E.row[E.cy], E.cx, c);
+  E.cx++;
+}
+
+void editorInsertNewline() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
+}
+
+void editorDelChar() {
+  if (E.cy == E.numrows) return;
+  if (E.cx == 0 && E.cy == 0) return;
+
+  erow *row = &E.row[E.cy];
+  if (E.cx > 0) {
+    editorRowDelChar(row, E.cx - 1);
+    E.cx--;
+  } else {
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
+  }
+}
+
+/*** file i/o ***/
+
+char *editorRowsToString(int *buflen) {
+  int totlen = 0;
+  for (int i = 0; i < E.numrows; ++i) {
+    totlen += E.row[i].size + 1;
+  }
+  *buflen = totlen;
+
+  // 两个指针指向同一个位置，不同于指针指向指针 pointer-to-pointer
+  // 这里巧妙的利用双指针来完成数据复制拼接的工作
+  // 指向base地址的buf指针就没有变过，所以最后它就指向的是全部的string数据
+  // 而p指针没复制一行，就指向当前buffer的末尾，方便第二次复制新的一行
+  char *buf = malloc(totlen);
+  char *p = buf;
+
+  for (int j = 0; j < E.numrows; ++j) {
+    memcpy(p, E.row[j].chars, E.row[j].size);
+    p += E.row[j].size;
+    *p = '\n';
+    p++;
+  }
+
+  return buf;
+}
+
+void editorOpen(char *filename) {
+  free(E.filename);
+  E.filename = strdup(filename);
+  FILE *fp = fopen(filename, "r");
+  if (!fp) {
+    die("fopen");
+  }
+
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' ||
+                           line[linelen - 1] == '\r'))
+      linelen--;
+    editorInsertRow(E.numrows, line, linelen);
+  }
+  free(line);
+  fclose(fp);
+  E.dirty = 0;
+}
+
+void editorSave() {
+  if (E.filename == NULL) {
+    E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    if (E.filename == NULL) {
+      editorSetStatusMessage("Save aborted");
+      return;
+    }
+  }
+  int len;
+  char *buf = editorRowsToString(&len);
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644); // 返回的是文件描述符
+  if (fd != -1) {
+    if (ftruncate(fd, len) != -1) {
+      if (write(fd, buf, len) == len) {
+        close(fd);
+        free(buf);
+        E.dirty = 0;
+        editorSetStatusMessage("%d bytes written to disk", len);
+      }
+      return;
+    }
+    close(fd);
+  }
+
+  free(buf);
+  editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
+/*** append buffer ***/
+
+typedef struct {
+  char *b;
+  int len;
+} abT;
+abT abuf;
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(abT *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(abT *ab) {
+  free(ab->b);
+}
+
+/*** Input ***/
+char *editorPrompt(char *prompt) {
+  size_t bufsize = 128;
+  char *buf = malloc(bufsize);
+  size_t buflen = 0;
+  buf[0] = '\0';
+  while (1) {
+    editorSetStatusMessage(prompt, buf);
+    editorRefreshScreen();
+    int c = editorReadKey();
+    if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE) {
+      if (buflen != 0) buf[--buflen] = '\0';
+    } else if (c == '\x1b') {
+      editorSetStatusMessage("");
+      free(buf);
+      return NULL;
+    } else if (c == '\r') {
+      if (buflen != 0) {
+        editorSetStatusMessage("");
+        return buf;
+      }
+    } else if (!iscntrl(c) && c < 128) {
+      if (buflen == bufsize - 1) {
+        bufsize *= 2;
+        buf = realloc(buf, bufsize);
+      }
+      buf[buflen++] = c;
+      buf[buflen] = '\0';
+    }
+  }
+}
+
+void editorMoveCursor(int key) {
+  // 定义一个指针，指向当前光标所在的行，这个指针的数据就是 erow struct结构的数据
+  const erow *row = (E.cy > E.numrows) ? NULL : &E.row[E.cy];
+
+  switch (key) {
+    case ARROW_LEFT:
+      if (E.cx != 0) {
+        E.cx--;
+      } else if (E.cy > 0) {
+        // 到达行首后，光标跳转到上一行的行尾
+        E.cy--;
+        E.cx = E.row[E.cy].size;
+      }
+      break;
+    case ARROW_RIGHT:
+      if (row && E.cx < row->size) {
+        E.cx++;
+      } else if (row && E.cy < E.screenrows) {
+        E.cy++;
+        E.cx = 0;
+      }
+      break;
+    case ARROW_UP:
+      if (E.cy != 0) {
+        E.cy--;
+      }
+      break;
+    case ARROW_DOWN:
+      if (E.cy < E.screenrows) {
+        E.cy++;
+      }
+      break;
+    default:
+      break;
+  }
+
+  // 光标不能超过当前行的长度
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
+  }
+}
+
+void editorProcessKeypress() {
+  int c = editorReadKey();
+  switch (c) {
+    case '\r':
+      editorInsertNewline();
+      break;
+    case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+
+      exit(0);
+    case CTRL_KEY('s'):
+      editorSave();
+      break;
+    case BACKSPACE:
+      // todo
+      break;
+    case DEL_KEY:
+      editorMoveCursor(ARROW_RIGHT);
+      editorDelChar();
+      break;
+    case PAGE_UP:
+    case PAGE_DOWN: {
+      if (c == PAGE_UP) {
+        E.cy = E.rowoff;
+      } else if (c == PAGE_DOWN) {
+        E.cy = E.rowoff + E.screenrows - 1;
+        if (E.cy > E.screenrows) E.cy = E.screenrows;
+      }
+      int times = E.screenrows;
+      while (times--) {
+        editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      }
+    }
+      break;
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+    case ARROW_UP:
+    case ARROW_DOWN:
+      editorMoveCursor(c);
+      break;
+    case '\x1b':
+      break;
+    default:
+      editorInsertChar(c);
+  }
+}
+
+/*** output ***/
+void editorScroll() {
+  E.rx = E.cx;
+  if (E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
+  if (E.cy < E.rowoff) {
+    E.rowoff = E.cy;
+  }
+  if (E.cy >= E.rowoff + E.screenrows) {
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
+  if (E.rx < E.coloff) {
+    E.coloff = E.rx;
+  }
+  if (E.rx >= E.coloff + E.screencols) {
+    E.coloff = E.rx - E.screencols + 1;
+  }
+}
+
+void editorDrawRows(abT *ab) {
+  for (int i = 0; i < E.screenrows; ++i) {
+    int filerow = i + E.rowoff;
+    if (filerow >= E.numrows) {
+      if (E.numrows == 0 && i == E.screenrows / 3) {
+        char welcome[80];
+        int welcomeLen = snprintf(welcome, sizeof welcome, "Kilo editor -- version %s", KILO_VERSION);
+        if (welcomeLen > E.screencols) {
+          welcomeLen = E.screencols;
+        }
+        int padding = (E.screencols - welcomeLen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomeLen);
+      } else {
+        abAppend(ab, "~", 1);
+      }
+    } else {
+      int len = E.row[filerow].rsize - E.coloff;
+      if (len < 0) len = 0;
+      if (len > E.screenrows) len = E.screenrows;
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);
+    }
+
+    abAppend(ab, "\x1b[K", 3);
+    abAppend(ab, "\r\n", 2);
+  }
+}
+
+void editorDrawStatusBar(abT *ab) {
+  abAppend(ab, "\x1b[7m", 4);
+  char status[80];
+  char rstatus[80];
+  int len = snprintf(status, sizeof status, "%.20s - %d lines %s", E.filename ? E.filename : "[NO NAME]", E.numrows,
+                     E.dirty ? "(modified)" : "");
+  int rlen = snprintf(rstatus, sizeof rstatus, "row:%d - col:%d", E.cy + 1, E.cx + 1);
+  if (len > E.screencols) len = E.screencols;
+  abAppend(ab, status, len);
+  while (len < E.screencols) {
+    // 显示在行尾
+    if (E.screencols - len == rlen) {
+      abAppend(ab, rstatus, rlen);
+      break;
+    } else {
+      abAppend(ab, " ", 1);
+      len++;
+    }
+  }
+  abAppend(ab, "\x1b[m", 3);
+  abAppend(ab, "\r\n", 2);
+}
+
+void editorDrawMessageBar(abT *ab) {
+  abAppend(ab, "\x1b[k", 3);
+  int msgLen = strlen(E.statusMsg);
+  if (msgLen > E.screencols) msgLen = E.screencols;
+  if (msgLen && time(NULL) - E.statusMsgTime < 5) {
+    abAppend(ab, E.statusMsg, msgLen);
+  }
+}
+
+void editorRefreshScreen() {
+  editorScroll();
+
+  abT ab = ABUF_INIT;
+
+  abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
+//    write(STDOUT_FILENO, "\x1b[2J", 4); //写入4bytes，0x1b, [, 2, J. 这是个escape sequence
+//    write(STDOUT_FILENO, "\x1b[H", 3); // place cursor to left-top
+
+  editorDrawRows(&ab);
+  editorDrawStatusBar(&ab);
+  editorDrawMessageBar(&ab);
+
+  char buf[32];
+  // 每次都将光标移到最初始的位置，然后由下面的代码再将光标移至指定位置
+  snprintf(buf, sizeof buf, "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
+  abAppend(&ab, buf, strlen(buf));
+
+  abAppend(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
+}
+
+// variadic function 可变参数, ... is ellipsis notation, make the function accept variable arguments
+// meaning it can take any number of arguments
+void editorSetStatusMessage(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf(E.statusMsg, sizeof E.statusMsg, fmt, ap);
+  va_end(ap);
+  E.statusMsgTime = time(NULL);
+}
+
+/*** init ***/
+void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+  E.rx = 0;
+  E.rowoff = 0;
+  E.coloff = 0;
+  E.numrows = 0;
+  E.dirty = 0;
+  E.row = NULL;
+  E.filename = NULL;
+  E.statusMsg[0] = '\0';
+  E.statusMsgTime = 0;
+
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+  E.screenrows -= 2;
+}
+
+int main(int argc, char *argv[]) {
+  enableRawMode();
+  initEditor();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
+
+  editorSetStatusMessage("HELP: Ctrl-s = save | Ctrl-q = quit");
+
+  while (1) {
+    editorRefreshScreen();
+    editorProcessKeypress();
+  }
+
+  return 0;
 }
